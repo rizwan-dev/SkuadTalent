02cb36cee22b61907dca4fe9c71a2ffd124c80ca
Shailee Sharma <shaileesharma2014@gmail.com>
2021-05-29 11:29:44 -0700

    Added logout option

--- app/src/main/java/com/skuad/talent/ui/main/dashboard/view/DashboardActivity.kt
+++ app/src/main/java/com/skuad/talent/ui/main/dashboard/view/DashboardActivity.kt
@@ -1,20 +1,37 @@
 package com.skuad.talent.ui.main.dashboard.view
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.adapters.ViewGroupBindingAdapter.setListener
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.skuad.talent.R
+import com.skuad.talent.base.common.Constant
 import com.skuad.talent.base.entities.ResourceState
 import com.skuad.talent.databinding.ActivityDashboardBinding
 import com.skuad.talent.domain.entities.dashboard.SkillsInfo
+import com.skuad.talent.extension.setSafeOnClickListener
 import com.skuad.talent.ui.base.BaseActivityVB
+import com.skuad.talent.ui.main.candiatedetails.view.CandidateDetailActivity
 import com.skuad.talent.ui.main.candidatelist.view.CandidateListActivity
 import com.skuad.talent.ui.main.candidatelist.view.CandidateListActivity.Companion.REQUEST_CHANGE_STATE
 import com.skuad.talent.ui.main.dashboard.adapter.DashboardAdapter
 import com.skuad.talent.ui.main.dashboard.vewmodel.DashboardViewModel
+import com.skuad.talent.ui.main.login.view.LoginActivity
 import com.skuad.talent.utils.GridSpacingItemDecoration
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.lougout_confirmation_dialog.*
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -23,6 +40,12 @@ class DashboardActivity : BaseActivityVB<ActivityDashboardBinding>() {
 
     @Inject
     lateinit var viewModel: DashboardViewModel
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+    private var mGoogleSignInOptions: GoogleSignInOptions? = null
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
 
     override fun attachBinding(
         list: MutableList<ActivityDashboardBinding>,
@@ -34,6 +57,15 @@ class DashboardActivity : BaseActivityVB<ActivityDashboardBinding>() {
     override fun setup() {
         setupObserver()
         getDashBoardData()
+        setUpLogout()
+    }
+
+    private fun setUpLogout() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     private fun getDashBoardData() {
@@ -72,6 +104,43 @@ class DashboardActivity : BaseActivityVB<ActivityDashboardBinding>() {
                     ), REQUEST_CHANGE_STATE
                 )
             }
+        }
+        iv_logout.setSafeOnClickListener {
+            showCnfDialog()
+
+        }
+    }
+
+    private fun showCnfDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.lougout_confirmation_dialog)
+        dialog?.window?.setBackgroundDrawableResource(R.drawable.round_corner_10)
+        dialog.setTitle(getString(R.string.logout_title))
+        dialog.setCancelable(true)
+        val textView: TextView = dialog.findViewById(R.id.tv_description)
+        textView.text = getString(R.string.cnf_logout)
+
+        val btnOk: TextView = dialog.findViewById(R.id.btn_ok_dialog)
+        btnOk.setOnClickListener {
+            logout()
+            setResult(RESULT_OK)
+            finish()
+            dialog.dismiss()
+        }
+        val btnCancle: TextView = dialog.findViewById(R.id.btn_cancle_dialog)
+        btnCancle.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    private fun logout() {
+        mGoogleSignInClient?.signOut()?.addOnCompleteListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            Timber.e("logout")
+            startActivity(intent)
+            finish()
+
         }
     }
 
